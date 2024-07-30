0c1a3206050748feeb50ce91b8f14a8b91b97fb1
edson <edson091990@gmail.com>
2024-07-30 11:12:45 -0300

    Expande função solicitarNumero para tratar entradas inválidas e inclui funcao solicitarOpcaoMenu

--- index.js
+++ index.js
@@ -17,29 +17,29 @@ function cadastrarAluno() {
 
 // 2 - CADASTRAR MATÉRIAS
 function cadastrarMaterias() {
-  //cadastrar no mínimo 3 matérias
-  //continuar cadastro até decidir parar
+  //exibe lista de alunos e solicita matrícula desejada
   exibirAlunos();
-  let matricula = solicitarMatricula();
+  let matricula = solicitarNumero('matricula');
+  //pesquisa aluno por matrícula
   alunos.filter(aluno => {
     if (matricula === aluno.matricula) {
-      let i = 1;
+      let i = aluno.materias.length;
       let continuar = 1;
       do {
         //solicitar no mínimo 3 matérias
-        if (i <= 3) {
+        if (i < 3) {
           console.log('Matéria ' + i);
           incluirMateria(aluno);
         } else {
           //verificar se deseja cadastrar mais matérias
-          continuar = +prompt('Deseja cadastrar outra matéria? Sim = 1, Não = 0: ');
+          continuar = solicitarNumero('continuar');
           if (continuar) {
             console.log('Matéria ' + i);
             incluirMateria(aluno);
           }
         }
         i++;
-      } while ((i <= 3) || continuar);
+      } while ((i < 3) || continuar);
     }
   })
 }
@@ -54,16 +54,16 @@ function incluirMateria(aluno) {
 function cadastrarNotas() {
   //solicitar 3 notas por matéria
   exibirAlunos();
-  let matricula = solicitarMatricula();
+  let matricula = solicitarNumero('matricula');
   // procurar aluno por matrícula
   alunos.filter(aluno => {
     if (matricula === aluno.matricula) {
       exibirMateriasCadastradas(matricula);
-      let numeroMateria = +prompt('Qual o número da matéria? ');
+      let numeroMateria = solicitarNumero('materia');
       // procurar matéria por número
       aluno.materias.filter(materia => {
         if (numeroMateria === materia.numero) {
-          // solicitar notas e cadastrar
+          // solicitar as 3 notas e cadastrar
           for (let i = 0; i < 3; i++) {
             let novaNota = +prompt(`Digite a nota ${i + 1}: `);
             materia['nota'+(i + 1)] = novaNota;
@@ -72,6 +72,29 @@ function cadastrarNotas() {
             Nota 1: ${materia.nota1} | Nota 2: ${materia.nota2} | Nota 3: ${materia.nota3}
             `);
           }
+        }
+      })
+    }
+  })
+}
+
+// 4 - CADASTRAR FALTAS
+function cadastrarFaltas() {
+  //exibe lista de alunos e solicita número de matrícula
+  exibirAlunos();
+  let matricula = solicitarNumero('matricula');
+  // procurar aluno por matrícula
+  alunos.filter(aluno => {
+    if (matricula === aluno.matricula) {
+      //exibe lista de matérias do aluno
+      exibirMateriasCadastradas(matricula);
+      let numeroMateria = solicitarNumero('materia');
+      //pesquisar matéria por número
+      aluno.materias.filter(materia => {
+        if (numeroMateria === materia.numero) {
+          //solicitar número de faltas por matéria
+          let numeroFaltas = +prompt('Quantas faltas deseja cadastrar? ');
+          materia.faltas += numeroFaltas;
         }
       })
     }
@@ -92,30 +115,10 @@ function exibirMateriasCadastradas(matricula) {
   })
 }
 
-// 4 - CADASTRAR FALTAS
-function cadastrarFaltas() {
-  exibirAlunos();
-  let matricula = solicitarMatricula();
-  // procurar aluno por matrícula
-  alunos.filter(aluno => {
-    if (matricula === aluno.matricula) {
-      //procurar matéria por número
-      exibirMateriasCadastradas(aluno);
-      let numeroMateria = +prompt('Qual o número da matéria? ');
-      aluno.materias.filter(materia => {
-        if (numeroMateria === materia.numero) {
-          //solicitar número de faltas por matéria
-          let numeroFaltas = +prompt('Quantas faltas deseja cadastrar? ');
-          materia.faltas += numeroFaltas;
-        }
-      })
-    }
-  })
-}
-
 // 5 - EXIBIR ALUNOS
 function exibirAlunos() {
   for(const aluno of alunos) {
+    //exibir nome e matrícula por aluno
     console.log(`Matrícula: ${aluno.matricula} | Nome: ${aluno.nome}`);
   }
   console.log('');
@@ -124,30 +127,34 @@ function exibirAlunos() {
 // 6 - EXIBIR RESULTADOS
 function exibirResultados() {
   exibirAlunos();
-  let matricula = solicitarMatricula();
+  let matricula = solicitarNumero('matricula');
+  //pesquisar aluno pela matrícula
   alunos.filter(aluno => {
     if(matricula === aluno.matricula) {
+      //exibir nome do aluno
       console.log('Aluno: ' + aluno.nome);
+      //exibir resultados por matéria
       exibirResultadoMateria(aluno);
     }
   })
 }
 
 function exibirResultadoMateria(aluno) {
-  //exibir média de cada matéria
   /*
-    indicar aluno aprovado/reprovado por matéria
-    (média das notas ou faltas)
+  indicar aluno aprovado/reprovado por matéria
+  (média das notas ou faltas)
   */
-  if(aluno.materias.length !== 0) {
-    aluno.materias.forEach(materia => {
-      let nota1 = materia.nota1;
-      let nota2 = materia.nota2;
-      let nota3 = materia.nota3;
-      let media = calcularMedia(nota1, nota2, nota3);
-      let faltas = materia.faltas;
-      console.log(`
-        ${materia.numero}. ${materia.nome}
+ if(aluno.materias.length !== 0) {
+   aluno.materias.forEach(materia => {
+     let nota1 = materia.nota1;
+     let nota2 = materia.nota2;
+     let nota3 = materia.nota3;
+     //cálculo da média
+     let media = calcularMedia(nota1, nota2, nota3);
+     let faltas = materia.faltas;
+     //exibir notas, média, faltas e situação de cada matéria
+     console.log(`
+      ${materia.numero}. ${materia.nome}
         Nota 1: ${nota1} | Nota 2: ${nota2} | Nota 3: ${nota3}
         Média: ${media} | Faltas: ${faltas}
         Situação: ${verificarSituacao(media, faltas)}
@@ -171,15 +178,26 @@ function verificarSituacao(media, faltas) {
   }
 }
 
-function solicitarMatricula() {
-  let matricula;
+function solicitarNumero(tipoSolicitacao) {
+  let valor;
   do {
-      matricula = +prompt('Qual a matrícula do aluno? ');
-      if (typeof(matricula) !== 'number') {
-        console.log('Entrada inválida. Digite apenas números.');
-      }
-    } while(typeof(matricula) !== 'number');
-  return matricula;
+    if (tipoSolicitacao === 'matricula') {
+      valor = +prompt('Qual o número da matrícula do aluno? ');
+    } else if (tipoSolicitacao === 'materia') {
+      valor = +prompt('Qual o número da matéria? ');
+    } else if (tipoSolicitacao === 'continuar') {
+      do {
+        valor = +prompt('Deseja cadastrar outra matéria? 1 - Sim | 0 - Não: ');
+        if (valor < 0 || valor > 1) {
+          console.log('Para Informar se deseja continuar, digite 1 para Sim ou 0 para Não');
+        }
+      } while (valor < 0 || valor > 1);
+    }
+    if (typeof(valor) !== 'number') {
+      console.log('Entrada inválida. Digite apenas números.');
+    }
+  } while (typeof(valor) !== 'number');
+  return valor;
 }
 
 // Menu
@@ -205,27 +223,45 @@ function main() {
     switch(opcao) {
       case 1:
         cadastrarAluno();
+        opcao = solicitarOpcaoMenu();
         break;
       case 2:
         cadastrarMaterias();
+        opcao = solicitarOpcaoMenu();
         break;
       case 3:
         cadastrarNotas();
+        opcao = solicitarOpcaoMenu();
         break;
       case 4:
         cadastrarFaltas();
+        opcao = solicitarOpcaoMenu();
         break;
       case 5:
         exibirAlunos();
+        opcao = solicitarOpcaoMenu();
         break;
       case 6:
         exibirResultados();
+        opcao = solicitarOpcaoMenu();
         break;
       case 0:
         console.log('Encerrando o sistema...');
         continuar = false;
     }
   } while(continuar);
+}
+
+function solicitarOpcaoMenu() {
+  let opcao;
+  console.log('Voltar ao menu? 1 - Sim | 0 - Não: ');
+  do {
+    opcao = +prompt('Opção: ');
+    if (opcao < 0 || opcao > 1) {
+      console.log('Digite 1 para voltar ao menu ou 0 para encerrar o sistema.');
+    }
+  } while (opcao < 0 || opcao > 1);
+  return opcao;
 }
 
 main();
