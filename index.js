74e67c34cfeda535fe593d94717530beafdad42a
Edson Rodrigues <rodriguesedson.dev@gmail.com>
2024-09-27 17:05:09 -0300

    feat: add function consultarAluno

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {listarAlunos, cadastrarAluno} = require('./src/js/funcoes-sistema.js');
+const {listarAlunos, cadastrarAluno, consultarAluno} = require('./src/js/funcoes-sistema.js');
 const prompt = require('prompt-sync')();
 
 function main() {
@@ -7,24 +7,31 @@ function main() {
 
   do {
     console.log(`
-    1 - Lista de alunos
-    2 - Cadastrar aluno
-    3 - Consultar cadastro de aluno
-    4 - Cadastrar matérias
-    5 - Inserir notas
-    6 - Inserir faltas
-    0 - Encerrar
+      \r==============Menu==============
+      \r1 - Lista de alunos
+      \r2 - Cadastrar aluno
+      \r3 - Consultar cadastro de aluno
+      \r4 - Cadastrar matérias
+      \r5 - Inserir notas
+      \r6 - Inserir faltas
+      \r0 - Encerrar
+      \r================================
     `);
     let opcao = +prompt('Selecione uma opção do menu: ');
     switch(opcao) {
       case 1:
+        console.log("\n========Lista de alunos========\n");
         listarAlunos();
         break;
       case 2:
-        cadastrarAluno();
+        console.log("\n=====Cadastro de novo aluno=====\n");
+        const nomeAluno = prompt('Qual o nome do aluno? ');
+        cadastrarAluno(nomeAluno);
         break;
       case 3:
-        consultarALuno();
+        console.log("\n========Consulta de aluno========\n");
+        const matricula = +prompt('Qual a matrícula do aluno? ');
+        consultarAluno(matricula);
         break;
       case 4:
         cadastrarMaterias();
--- src/js/funcoes-sistema.js
+++ src/js/funcoes-sistema.js
@@ -4,26 +4,34 @@ const prompt = require('prompt-sync')();
 function listarAlunos() {
   const db = conectarDb();
   const alunos = db.file.alunos;
+
   if (alunos.length === 0) {
     console.log("\nSem alunos cadastrados...")
   } else {
-    console.log("\nLista de alunos:\n")
     alunos.forEach(aluno => {
       console.log(`Matrícula: ${aluno.matricula} - Nome: ${aluno.nome}`);
     })
   }
 }
 
-function cadastrarAluno() {
+function cadastrarAluno(nomeAluno) {
   const db = conectarDb();
   const matricula = db.file.alunos.length + 1;
-  const nomeAluno = prompt('Qual o nome do aluno? ');
   const novoAluno = criarAluno(matricula, nomeAluno);
 
   db.file.alunos.push(novoAluno);
-
   fs.writeFileSync(db.src, JSON.stringify(db.file))
   console.log('Registro de aluno criado.')
+}
+
+function consultarAluno(matricula) {
+  const db = conectarDb();
+  const alunos = db.file.alunos;
+  const aluno = alunos.filter(aluno => {
+    if (aluno.matricula === matricula) return aluno;
+  });
+
+  exibirDadosAluno(aluno[0]);
 }
 
 function conectarDb() {
@@ -41,4 +49,34 @@ function criarAluno(matricula, nomeAluno) {
   }
 }
 
-module.exports = {listarAlunos, cadastrarAluno};
+function exibirDadosAluno(aluno) {
+  if (aluno) {
+    console.log(`
+    \r=================================
+    \rMatrícula: ${aluno.matricula}
+    \rNome: ${aluno.nome}
+    \rMatérias:
+    \r  ${exibirMaterias(aluno)}
+    \r=================================
+    `);
+  } else {
+    console.log("Não há aluno cadastrado com a matrícula informada.");
+  }
+}
+
+function exibirMaterias(aluno) {
+  if (aluno.materias) {
+    aluno.materias.forEach(item => {
+      return `
+      Nome: ${item.nome}  
+      Nota 1: ${item.nota1}
+      Nota 2: ${item.nota2}
+      Nota 3: ${item.nota3}
+      `
+    })
+  } else {
+    return "Matérias pendentes de cadastro."
+  }
+}
+
+module.exports = {listarAlunos, cadastrarAluno, consultarAluno};
